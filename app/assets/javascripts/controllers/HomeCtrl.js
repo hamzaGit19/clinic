81a05cc733fd3d40c148aae01d6e880da55d6918
Hamza Awais <hamza.awais@square63.org>
2020-07-04 16:42:09 +0500

    Added slick with CDN.

--- app/assets/javascripts/controllers/HomeCtrl.js
+++ app/assets/javascripts/controllers/HomeCtrl.js
@@ -2,4 +2,10 @@ angular
   .module('clinic')
   .controller('HomeCtrl', function($scope, $rootScope, Auth){
     $scope.hello = "Hello World"
+    $('.withOptions').slick({
+      dots: true,
+      arrows: true,
+      autoplay: true,
+      autoplayspeed: 1000,
+   });
   })
